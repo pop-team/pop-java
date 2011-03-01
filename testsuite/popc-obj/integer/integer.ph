149483e8d2f97ea20de0c311599442fd7e1c1918
valentin.clement <valentin.clement@523ada35-c2af-49ce-adc4-a38780473c2f>
2011-03-01 12:14:49 +0000

    - Push current version on SVN
    git-svn-id: https://forge.tic.eia-fr.ch/svn/eiafr/tic/rad/info/popjava/dev/trunk@164 523ada35-c2af-49ce-adc4-a38780473c2f

---
+++ testsuite/popc-obj/integer/integer.ph
@@ -0,0 +1,13 @@
+parclass Integer
+{
+	classuid(1500);
+public:
+	Integer();
+	~Integer();
+	mutex void Add (Integer &other);
+	conc int Get ();
+	seq async void Set(int val);
+private:
+	int data;
+};
+
---
+++ testsuite/popc-obj/jinteger/main.cc
@@ -0,0 +1,40 @@
+
+
+
+#include "integer.ph"
+#include "integer2.ph"
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	try
+	{
+		// Create 2 Integer objects
+		Integer2 i1;
+		Jinteger j1;
+
+		// Set values
+		i1.Set(1); j1.Set(2);
+
+		printf("i1=%d, j1=%d\n", i1.Get(), j1.Get());
+
+		printf("Add j1 to i1\n");
+		
+		i1.Add(j1);
+		
+		printf("j1+i1=%d", i1.Get()); 
+
+	} // Try
+
+	catch (POPException *e)
+	{
+		printf("Exception occurs in application :\n");
+		e->Print();
+		delete e;
+		return -1;
+	} // catch
+
+	return 0;
+} //main
