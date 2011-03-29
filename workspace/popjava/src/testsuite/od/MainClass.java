859e0e65431ba8e4ea05da2f29dbb8780754aae4
valentin.clement <valentin.clement@523ada35-c2af-49ce-adc4-a38780473c2f>
2011-03-29 15:04:23 +0000

    - Add new testcase
    git-svn-id: https://forge.tic.eia-fr.ch/svn/eiafr/tic/rad/info/popjava/dev/trunk@542 523ada35-c2af-49ce-adc4-a38780473c2f

---
+++ workspace/popjava/src/testsuite/od/MainClass.java
@@ -0,0 +1,18 @@
+package testsuite.od;
+import popjava.PopJava;
+import popjava.base.POPException;
+import popjava.system.POPSystem;
+
+public class MainClass {
+	public static void main(String... args){
+		try{
+			POPSystem.initialize(args);
+			System.out.println("Create object without JobMgr");
+			ParObj po = (ParObj)PopJava.newActive(ParObj.class);
+			System.out.println(po.getAccessPoint().toString());
+			System.out.println("Creation successful");
+		} catch(POPException e) {
+			System.err.println("POP-Java exception catched :"+e.errorMessage);
+		}
+	}
+}
---
+++ workspace/popjava/src/testsuite/od/ParObj.java
@@ -0,0 +1,11 @@
+package testsuite.od;
+
+import popjava.base.POPObject;
+
+public class ParObj extends POPObject  {
+	public ParObj()  {
+		Class<?> c = ParObj.class;
+		initializePOPObject(c);
+		od.setHostname("localhost");
+	}
+}
