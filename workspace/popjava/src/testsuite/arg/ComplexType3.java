62ac6f8b9a75c5e6356a31f301fbfb779fd05fb0
Valentin Clément <valentin.clement@hefr.ch>
2011-04-06 12:23:51 +0000

    - Add forgotten source files

---
+++ workspace/popjava/src/testsuite/arg/ComplexType3.java
@@ -0,0 +1,43 @@
+package testsuite.arg;
+
+import java.util.Vector;
+
+import popjava.buffer.Buffer;
+import popjava.dataswaper.IPOPBase;
+
+public class ComplexType3 implements IPOPBase {
+	private Vector<Integer> vector;
+	private int value;
+	
+	public ComplexType3(){
+		vector = new Vector<Integer>(10);
+		for (int i = 0; i < vector.capacity(); i++) {
+			vector.add(new Integer(i));
+		}
+		value = vector.size();
+	}
+		
+	@Override
+	public boolean deserialize(Buffer buffer) {
+		value = buffer.getInt();
+		int size = buffer.getInt();
+		int[] array = buffer.getIntArray(size);
+		vector = new Vector<Integer>(size);
+		for (int i = 0; i < array.length; i++) {
+			vector.add(new Integer(array[i]));
+		}
+		return true;
+	}
+	
+	@Override
+	public boolean serialize(Buffer buffer) {
+		int size = value;
+		buffer.putInt(size);
+		int[] array = new int[vector.size()];
+		for (int i = 0; i < array.length; i++) {
+			array[i] = vector.get(i).intValue();
+		}
+		buffer.putIntArray(array);
+		return true;
+	}
+}
