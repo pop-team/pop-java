6cee6471e6ad6a4180bf1eb9c8b2d5e9da530444
valentin.clement <valentin.clement@523ada35-c2af-49ce-adc4-a38780473c2f>
2011-04-06 12:28:49 +0000

    - Add forgotten source files
    git-svn-id: https://forge.tic.eia-fr.ch/svn/eiafr/tic/rad/info/popjava/dev/trunk@809 523ada35-c2af-49ce-adc4-a38780473c2f

---
+++ workspace/popjava/src/testsuite/multiobj/MyObj4.java
@@ -0,0 +1,27 @@
+package testsuite.multiobj;
+
+import popjava.base.POPObject;
+import popjava.base.Semantic;
+
+
+public class MyObj4 extends POPObject{
+	protected int data;
+	
+	public MyObj4(){
+		setClassId(1211);
+		Class<?> c = MyObj4.class;
+		od.setHostname("localhost");
+		initializePOPObject(c);
+		addSemantic(c, "set",	Semantic.Sequence | Semantic.Synchronous);
+		addSemantic(c, "get",	Semantic.Concurrent | Semantic.Synchronous);
+		
+	}
+	
+	public void set(int value) {
+		data = value;
+	}
+	
+	public int get(){
+		return data + 1000;
+	}
+}
