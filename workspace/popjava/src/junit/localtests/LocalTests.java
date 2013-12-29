7229874d36d9bfe8c11e53832ce75666a7b8bbf0
Beat Wolf <beat.wold@hefr.ch>
2013-12-29 17:00:45 +0000

    test more

--- workspace/popjava/src/junit/localtests/LocalTests.java
+++ workspace/popjava/src/junit/localtests/LocalTests.java
@@ -3,12 +3,13 @@ package junit.localtests;
 import junit.localtests.annotations.AnnotationsTest;
 import junit.localtests.callback.CallBackTest;
 import junit.localtests.integer.IntegerTest;
+import junit.localtests.readerWriter.ReaderWriterTest;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses( { CallBackTest.class, IntegerTest.class, AnnotationsTest.class})
+@Suite.SuiteClasses( { CallBackTest.class, IntegerTest.class, AnnotationsTest.class, ReaderWriterTest.class})
 public class LocalTests {
 
 }
