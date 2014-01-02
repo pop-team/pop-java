bf35a67cef435c3b28198cec10c7c3665067bc69
Beat Wolf <beat.wold@hefr.ch>
2014-01-02 11:14:09 +0000

    add missing files

---
+++ workspace/popjava/src/junit/localtests/readerWriter/Reader.java
@@ -0,0 +1,18 @@
+package junit.localtests.readerWriter;
+
+import java.util.List;
+
+public class Reader {
+	
+	private List<Worker> workers;
+	public Reader(List<Worker> workers){
+		this.workers = workers;
+	}
+
+	public void work(){
+		for(Worker work: workers){
+			work.work();
+		}
+	}
+	
+}
