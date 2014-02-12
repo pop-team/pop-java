b135fc55ce2395165bd8520d9ac3d588f241ac39
beat.wolf <beat.wolf@523ada35-c2af-49ce-adc4-a38780473c2f>
2014-02-12 14:07:37 +0000

    Lower request queue peeking intervall
    git-svn-id: https://forge.tic.eia-fr.ch/svn/eiafr/tic/rad/info/popjava/dev/trunk@21084 523ada35-c2af-49ce-adc4-a38780473c2f

--- workspace/popjava/src/popjava/broker/Broker.java
+++ workspace/popjava/src/popjava/broker/Broker.java
@@ -40,7 +40,7 @@ public class Broker {
 	static public final int Running = 0;
 	static public final int Exit = 1;
 	static public final int Abort = 2;
-	static public final int REQUEST_QUEUE_TIMEOUT_MS = 1000;
+	static public final int REQUEST_QUEUE_TIMEOUT_MS = 100;
 	static public final int BasicCallMaxRange = 10;
 	static public final int ConstructorSemanticId = 21;
 	static public final String CallBackPrefix = "-callback=";
