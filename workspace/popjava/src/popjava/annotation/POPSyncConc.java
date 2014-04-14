921d3aa95e6caf35eebd6381a5bbf08a7d2f79c8
beatwolf <wolf@fryx.ch>
2014-04-14 22:30:07 +0200

    Added more javadoc

--- workspace/popjava/src/popjava/annotation/POPSyncConc.java
+++ workspace/popjava/src/popjava/annotation/POPSyncConc.java
@@ -5,6 +5,14 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Defines a method as synchronous and concurrent.
+ * Synchronous method calls wait for the call to finish before continuing the execution of the calling code.
+ * Concurrent methods can be called in parallel by multiple processes. They can not be executed
+ * if a mutex call is currently being executed on the target object
+ * @author Beat Wolf
+ *
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface POPSyncConc {
--- workspace/popjava/src/popjava/annotation/POPSyncMutex.java
+++ workspace/popjava/src/popjava/annotation/POPSyncMutex.java
@@ -5,6 +5,13 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Defines a method as synchronous and mutex.
+ * Synchronous method calls wait for the call to finish before continuing the execution of the calling code.
+ * Mutex methods can only be executed when no other method is executed on the same object.
+ * @author Beat Wolf
+ *
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface POPSyncMutex {
