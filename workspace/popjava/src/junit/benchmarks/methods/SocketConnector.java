a31524954746f1ef3e17ac4dc58bf84275238a92
beatwolf <wolf@fryx.ch>
2014-04-17 22:43:08 +0200

    Added first Socket based benchmarks for a baseline comparison

--- workspace/popjava/src/junit/benchmarks/methods/SocketConnector.java
+++ workspace/popjava/src/junit/benchmarks/methods/SocketConnector.java
@@ -1,5 +1,54 @@
 package junit.benchmarks.methods;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class SocketConnector {
+	
+	private final Socket socket;
+	private final OutputStream out;
+	private final InputStream in;
+	
+	public SocketConnector() throws UnknownHostException, IOException{
+		socket = new Socket("localhost", SocketServer.PORT);
+		out = socket.getOutputStream();
+		in = socket.getInputStream();
+	}
+	
+	public void noParamNoReturn() throws IOException{
+		out.write(1);
+	}
+	
+	public int noParamSimple() throws IOException{
+		out.write(2);
+		return in.read();
+	}
+	
+	public String [] noParamComplex() throws IOException{
+		out.write(3);
+		int strings = in.read();
+		String [] temp = new String[strings];
+		
+		for(int i = 0; i < temp.length; i++){
+			int l = in.read();
+			
+			byte [] t = new byte[l];
+			
+			while(l > 0){
+				l -= in.read(t, l, t.length - l);
+			}
+			
+			temp[i] = new String(t);
+		}
+		
+		return temp;
+	}
+	
+	public void close() throws IOException{
+		socket.close();
+	}
 
 }
--- workspace/popjava/src/junit/benchmarks/methods/SocketServer.java
+++ workspace/popjava/src/junit/benchmarks/methods/SocketServer.java
@@ -1,5 +1,52 @@
 package junit.benchmarks.methods;
 
-public class SocketServer {
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class SocketServer implements Runnable{
+
+	public static final int PORT = 5543;
+	
+	private static String [] complexReturn = new String[]{"asdfasdf", "asdfasdf", "asdfasdf"};
+	
+	@Override
+	public void run() {
+		ServerSocket serverSocket;
+		try {
+			serverSocket = new ServerSocket(PORT);
+			Socket socket = serverSocket.accept();
+			
+			InputStream in = socket.getInputStream();
+			OutputStream out = socket.getOutputStream();
+			int data = -1;
+			while((data = in.read()) != -1 && !Thread.interrupted()){
+				switch(data){
+				case 1:
+					break;
+				case 2:
+					out.write(100);
+					break;
+				case 3:
+					out.write(complexReturn.length);
+					for(String s: complexReturn){
+						out.write(s.length());
+						out.write(s.getBytes());
+					}
+					break;
+				}
+			}
+			
+			serverSocket.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		
+	}
+	
+	
 
 }
