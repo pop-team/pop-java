a8096b278079fda7191da93bc3bfab7b15eda3d9
beatwolf <wolf@fryx.ch>
2014-04-17 22:05:19 +0200

    Fixed non fatal exception when loading popjavaapp service added more benchmarks to method benchmark suite

--- workspace/popjava/src/junit/benchmarks/methods/TestMethods.java
+++ workspace/popjava/src/junit/benchmarks/methods/TestMethods.java
@@ -1,5 +1,7 @@
 package junit.benchmarks.methods;
 
+import static org.junit.Assert.*;
+
 import org.junit.Test;
 
 import popjava.PopJava;
@@ -25,4 +27,35 @@ public class TestMethods {
 		POPSystem.end();
 	}
 	
+	@Test
+	public void testPOPNoParamSimple(){
+		POPSystem.initialize();
+		POPMethods object = PopJava.newActive(POPMethods.class);
+		
+		long start = System.currentTimeMillis();
+		
+		for(int i = 0; i < REPETITIONS; i++){
+			assertEquals(100, object.noParamSimple());
+		}
+		
+		System.out.println("testPOPNoParamSimple() "+(System.currentTimeMillis() - start)+" ms");
+		
+		POPSystem.end();
+	}
+	
+	@Test
+	public void testPOPNoParamComples(){
+		POPSystem.initialize();
+		POPMethods object = PopJava.newActive(POPMethods.class);
+		
+		long start = System.currentTimeMillis();
+		
+		for(int i = 0; i < REPETITIONS; i++){
+			assertEquals(3, object.noParamComplex().length);
+		}
+		
+		System.out.println("testPOPNoParamComplex() "+(System.currentTimeMillis() - start)+" ms");
+		
+		POPSystem.end();
+	}
 }
