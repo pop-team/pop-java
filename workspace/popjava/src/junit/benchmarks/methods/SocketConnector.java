c8e0440263a24a034ecc6f155a182a987df6243e
beatwolf <wolf@fryx.ch>
2014-04-18 09:17:32 +0200

    Working basic socket tests

--- workspace/popjava/src/junit/benchmarks/methods/SocketConnector.java
+++ workspace/popjava/src/junit/benchmarks/methods/SocketConnector.java
@@ -1,5 +1,7 @@
 package junit.benchmarks.methods;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -14,31 +16,35 @@ public class SocketConnector {
 	
 	public SocketConnector() throws UnknownHostException, IOException{
 		socket = new Socket("localhost", SocketServer.PORT);
-		out = socket.getOutputStream();
-		in = socket.getInputStream();
+		out = new BufferedOutputStream(socket.getOutputStream());
+		in = new BufferedInputStream(socket.getInputStream());
 	}
 	
 	public void noParamNoReturn() throws IOException{
 		out.write(1);
+		out.flush();
 	}
 	
 	public int noParamSimple() throws IOException{
 		out.write(2);
+		out.flush();
 		return in.read();
 	}
 	
 	public String [] noParamComplex() throws IOException{
 		out.write(3);
+		out.flush();
+		
 		int strings = in.read();
 		String [] temp = new String[strings];
 		
 		for(int i = 0; i < temp.length; i++){
 			int l = in.read();
-			
 			byte [] t = new byte[l];
 			
 			while(l > 0){
-				l -= in.read(t, l, t.length - l);
+				int dataRead = in.read(t, t.length - l, l);
+				l -= dataRead;
 			}
 			
 			temp[i] = new String(t);
