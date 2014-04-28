c84120f2bf272ab836fbefeb426cff708918ef0f
beat.wolf <beat.wolf@523ada35-c2af-49ce-adc4-a38780473c2f>
2014-04-28 13:41:41 +0000

    Added currently failing tests for enums as parameters to constructors and methods
    git-svn-id: https://forge.tic.eia-fr.ch/svn/eiafr/tic/rad/info/popjava/dev/trunk@21448 523ada35-c2af-49ce-adc4-a38780473c2f

---
+++ workspace/popjava/src/junit/localtests/enums/EnumRemoteObject.java
@@ -0,0 +1,43 @@
+package junit.localtests.enums;
+
+import popjava.annotation.POPClass;
+import popjava.annotation.POPObjectDescription;
+import popjava.annotation.POPSyncMutex;
+import popjava.base.POPObject;
+
+@POPClass
+public class EnumRemoteObject extends POPObject{
+
+	public enum Test{
+		A,
+		B,
+		C;
+	};
+	
+	private Test constructor;
+	private Test method;
+	
+	public EnumRemoteObject(){
+		
+	}
+	
+	@POPObjectDescription(url = "localhost")
+	public EnumRemoteObject(Test param){
+		constructor = param;
+	}
+	
+	@POPSyncMutex
+	public void setEnum(Test param){
+		method = param;
+	}
+	
+	@POPSyncMutex
+	public Test getConstructor(){
+		return constructor;
+	}
+	
+	@POPSyncMutex
+	public Test getMethod(){
+		return method;
+	}
+}
