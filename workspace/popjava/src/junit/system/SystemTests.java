c8dc310494c5166b13d8697d04ec87ea6afb75b3
beat.wolf <beat.wolf@hefr.ch>
2016-07-19 11:08:35 +0200

    Fix issue with last commit

--- workspace/popjava/src/junit/system/SystemTests.java
+++ workspace/popjava/src/junit/system/SystemTests.java
@@ -4,7 +4,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses( { POPSystemTest.class, RawBufferTest.class, ConstructorTests.class, POPJavaDeamonTest.class, POPObjectTest.class})
+@Suite.SuiteClasses( { POPSystemTest.class, RawBufferTest.class, ConstructorTests.class, POPJavaDeamonTest.class})
 public class SystemTests {
     
 }
