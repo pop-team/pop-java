437b7d3c1f7d889957d4947eb0b923a4a9e23c53
Beat Wolf <beat.wolf@hefr.ch>
2017-05-11 19:08:38 +0200

    formatting

--- workspace/popjava/src/junit/localtests/referencePassing/ReferenceTest.java
+++ workspace/popjava/src/junit/localtests/referencePassing/ReferenceTest.java
@@ -9,6 +9,11 @@ import popjava.system.POPSystem;
 
 public class ReferenceTest {
 
+/**
+ * THis test randomly fails.
+ * THis is most likely because the object B created in getB() is destroyed too soon
+ * 
+ */
 	@Test
 	public void test(){
 		POPSystem.start();
